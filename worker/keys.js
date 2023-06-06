e9846312a9ae9ebbd12aa11f7994bd51fcc32964
Pawat Suntharanund <pawat.suntharanund@gmail.com>
2023-06-06 14:24:26 -0500

    initial commit

---
+++ worker/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT
+};
+
+//this is for the host name and port to connect to our application from redis
