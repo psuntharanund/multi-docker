ec90c420a5565f76c70b28c60bc5a941dfba5ab1
Pawat Suntharanund <pawat.suntharanund@gmail.com>
2023-06-07 16:01:28 -0500

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React, you're doing a great job Ohm!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
